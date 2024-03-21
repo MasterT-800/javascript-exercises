b4368249c15c7de0fbfcf9ef38bb0fe2ca1d7869
tlinux <115206437+MasterT-800@users.noreply.github.com>
2024-03-21 15:46:57 -0400

    Fullfilled test criteria

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    //Set text to lowercase then replace all non alphanumeric characters with empty character
+    let simpleText = text.toLowerCase().replace(/[^a-z0-9]/g, "");
+    //Split the text up to individual characters reverse them then join them back together
+    let reverse = simpleText.split('').reverse().join('');
+    //If text equals reverse text return true otherwise return false
+    return (simpleText == reverse);
 };
 
 // Do not edit below this line
