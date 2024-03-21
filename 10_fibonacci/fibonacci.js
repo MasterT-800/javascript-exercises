086fd35284de90cfa9cfc06d59edf89f66ea2838
tlinux <115206437+MasterT-800@users.noreply.github.com>
2024-03-21 16:23:48 -0400

    Met all test criteria

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+//Using recursion function will continue to call itself until it equals 1, then it will
+//continue until it reaches the input value
+const fibonacci = function calculateFib(member) {
+    if (member < 0){
+        return "OOPS";
+    }
+    if (member == 0){
+        return 0;
+    }
+    if (member == 1){
+        return 1;
+    }
+    return (calculateFib(member - 1) + calculateFib(member - 2));
 };
+
+console.log(fibonacci(6));
 
 // Do not edit below this line
 module.exports = fibonacci;
