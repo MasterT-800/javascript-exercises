2a048fc74a2e9cd1444fdc558b97e09ba5a2bc9d
tlinux <115206437+MasterT-800@users.noreply.github.com>
2024-03-25 16:18:52 -0400

    Met all test criteria

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+  let length = array.length;
+  // If person has not died set death date to current date
+  for (let i =0; i < length; i++) {
+    if ("yearOfDeath" in array[i] == false) {
+      array[i].yearOfDeath = ((new Date()).getFullYear());
+    }
+}
+  //Sort people by oldest to youngest and return the oldest person object
+    const oldest = array.sort(function(a, b) {
+        const lastInventor = a.yearOfDeath - a.yearOfBirth;
+        const nextInventor = b.yearOfDeath - b.yearOfBirth;
+        return lastInventor > nextInventor ? -1 : 1;
+      });
+    return oldest[0];
 };
 
 // Do not edit below this line
